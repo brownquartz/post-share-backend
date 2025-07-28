52ea7cf17f75af9c21577bcdbe05a3bc495af782
brownquartz <parkcm2262@gmail.com>
2025-07-28 09:42:06 +0900

    for container check

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ const app  = express();
 // const PORT = process.env.PORT;
 const port = parseInt(process.env.PORT, 10);
 
+app.use((req, res, next) => {
+  console.log(`▶ REQUEST ${req.method} ${req.url}`);
+  next();
+});
+
 // CORS
 app.use(cors());
 app.options('*', cors());
+
+app.get('/test', (_req, res) => {
+  console.log('● /test called');
+  res.status(200).send('OK');
+});
 
 // 1) ヘルスチェック: アンダースコア２個に修正
 app.get('/__health', (req, res) => {
@@ -21,14 +31,14 @@ app.get('/__health', (req, res) => {
 app.use(express.json());
 app.use('/api/posts', postsRouter);
 
-// 3) 静的ファイル配信: build は同階層なので __dirname + '/build'
-const clientBuildPath = path.join(__dirname, 'build');
-app.use(express.static(clientBuildPath));
+// // 3) 静的ファイル配信: build は同階層なので __dirname + '/build'
+// const clientBuildPath = path.join(__dirname, 'build');
+// app.use(express.static(clientBuildPath));
 
-// 4) それ以外は index.html へフォールバック
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(clientBuildPath, 'index.html'));
-});
+// // 4) それ以外は index.html へフォールバック
+// app.get('*', (_req, res) => {
+//   res.sendFile(path.join(clientBuildPath, 'index.html'));
+// });
 
 // 5) サーバー起動
 console.log('▶ LISTENING ON PORT', port);
